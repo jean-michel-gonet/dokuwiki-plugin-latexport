8b913a548ba8a08d63eefcbfb89392689342b8a4
Jean-Michel Gonet <jmgonet@yahoo.com>
2017-08-27 07:54:18 +0000

    #3 - Rendu basique des titres et sous-titres

--- renderer/tex.php
+++ renderer/tex.php
@@ -13,46 +13,77 @@ if(!defined('DOKU_INC')) die();
  */
 class renderer_plugin_latexport_tex extends Doku_Renderer {
 
-    /**
-     * Returns the format produced by this renderer.
-     */
-    function getFormat(){
-        return "tex";
-    }
+	/**
+	 * Returns the format produced by this renderer.
+	 */
+	function getFormat(){
+		return "tex";
+	}
 
-    /**
-     * Do not make multiple instances of this class
-     */
-    function isSingleton(){
-        return true;
-    }
+	/**
+	 * Do not make multiple instances of this class
+	 */
+	function isSingleton(){
+		return true;
+	}
 
-    /**
-     * Initialize the rendering
-     */
-    function document_start() {
-        global $ID;
+	/**
+	 * Initialize the rendering
+	 */
+	function document_start() {
+		global $ID;
 
-        // Create HTTP headers
-        $output_filename = str_replace(':','-',$ID).'.tex';
-        $headers = array(
-            'Content-Type' => 'application/x-tex',
-            'Content-Disposition' => 'attachment; filename="'.$output_filename.'";',
-        );
+		// Create HTTP headers
+		$output_filename = str_replace(':','-',$ID).'.tex';
+		$headers = array(
+				'Content-Type' => 'application/x-tex',
+				'Content-Disposition' => 'attachment; filename="'.$output_filename.'";',
+				);
 
-        // store the content type headers in metadata
-        p_set_metadata($ID,array('format' => array('latexport_tex' => $headers) ));
+		// store the content type headers in metadata
+		p_set_metadata($ID,array('format' => array('latexport_tex' => $headers) ));
 
-	// Starts the document:
-	$this->doc = '\documentclass{article}
-\begin{document}
-';
-    }
+		// Starts the document:
+		$this->command('documentclass', 'book');
+		$this->command('begin', 'document');
+	}
 
-    /**
-     * Closes the document
-     */
-    function document_end(){
-	$this->doc .= '\end{document}';
-    }
+	/**
+	 * Headers are transformed in part, chapter, section, subsection and subsubsection.
+	 */
+	function header($text, $level, $pos) {
+		switch($level) {
+			case 1:
+				$this->command('part', $text);
+				break;
+			case 2:
+				$this->command('chapter', $text);
+				break;
+			case 3:
+				$this->command('section', $text);
+				break;
+			case 4:
+				$this->command('subsection', $text);
+				break;
+			default:
+				$this->command('subsubsection', $text);
+				break;
+		}
+	}
+
+	/**
+	 * Closes the document
+	 */
+	function document_end(){
+		$this->command('end', 'document');
+	}
+
+	/**
+	 * Adds a latex command to the document.
+	 * @param name Command name.
+	 * @param argument To be included in curly brackets.
+	 */
+	function command($name, $argument) {
+		$this->doc .= '\\'.$name.'{'.$argument."}\r\n";
+	}
 }
